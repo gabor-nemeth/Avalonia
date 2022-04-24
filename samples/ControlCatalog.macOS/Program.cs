3f78cf751d9aa97182a241864b46ec13963cb51a
Gabor Nemeth <37656640+gabor-nemeth@users.noreply.github.com>
2022-04-24 03:45:10 +0200

    add macOS sample using net6.0-macos target

---
+++ samples/ControlCatalog.macOS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Dialogs;
+using Avalonia.LogicalTree;
+using Avalonia.Threading;
+using ControlCatalog;
+
+var builder = BuildAvaloniaApp();
+builder.StartWithClassicDesktopLifetime(args, ShutdownMode.OnMainWindowClose);
+
+/// <summary>
+/// This method is needed for IDE previewer infrastructure
+/// </summary>
+static AppBuilder BuildAvaloniaApp()
+    => AppBuilder.Configure<App>()
+        .UsePlatformDetect()
+        .UseSkia()
+        .UseManagedSystemDialogs()
+        .AfterSetup(builder =>
+        {
+            builder.Instance!.AttachDevTools(new Avalonia.Diagnostics.DevToolsOptions() { StartupScreenIndex = 1, });
+        })
+        .LogToTrace();
